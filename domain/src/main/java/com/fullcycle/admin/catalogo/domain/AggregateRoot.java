046a4b8e88e9df4e20ef0a17aad40a86978dc988
vinicamposdev <viniciusmoliveirac@hotmail.com>
2022-11-14 17:05:54 -0300

    feat: :sparkles: add entities, value object and agregate in domain

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/AggregateRoot.java
@@ -0,0 +1,7 @@
+package com.fullcycle.admin.catalogo.domain;
+
+public class AggregateRoot <ID extends Identifier> extends Entity<ID> {
+    protected AggregateRoot(final ID id) {
+        super(id);
+    }
+}
